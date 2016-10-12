3cb9d0c42a2f2f61329911c9906801437f3eda81
hellbertos <james.ruggieri@yahoo.com>
2016-10-11 18:23:47 -0700

    Update and complete intro message and Skills at a Glance section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,7 +17,7 @@ var bio = {
         "location" : "San Francisco"
     },
     "bioPic" : "images/gravitar.jpg",
-    "welcomeMsg" : "I am a web designer and front end web developer with a passion for creating accessible, informative and aesthetically pleasing web sites. I am continually striving to learn and progress in all phases of the craft. When I'm not coding or pondering the latest web trends, techniques and advancements, I enjoy: a multitude of activities and outdoor endeavors, motorcycle restoration projects, home-brewing and learning to play guitar. Thanks for stopping by and don't hesitate to contact me via the email link above.",
+    "welcomeMsg" : "I am a web designer and front end web developer with a passion for creating functional, accessible, informative and aesthetically pleasing web sites. I am continually striving to learn and progress in all phases of the craft. Currently I'm studying React and ES6 as well as taking online instruction to build my first Wordpress plugin.<br><br>When I'm not coding or pondering the latest web trends, techniques and advancements, I enjoy: a multitude of activities and outdoor endeavors, motorcycle restoration projects, home-brewing and learning to play guitar. Thanks for stopping by and don't hesitate to contact me via the email link above.",
     "skills" : [
         '<img class="skillsIco htmlSkill" src="./images/ico-html5.svg">',
         '<img class="skillsIco cssSkill" src="./images/ico-css3.svg">',
@@ -289,7 +289,7 @@ $('.htmlSkill').tipso({
   offsetX           : 0,
   offsetY           : 0,
   tooltipHover      : true,
-  content           : 'I enjoy creating clean semantic html templates and love figuring out challenging design layouts',
+  content           : 'I enjoy creating clean semantic html templates and integrating modern HTML5 tags; especially the &#60;picture&#62; tag for <em>image heavy</em> responsive sites.',
   ajaxContentUrl    : null,
   contentElementId  : null,
   useTitle          : false,
@@ -317,7 +317,7 @@ $('.cssSkill').tipso({
   offsetX           : 0,
   offsetY           : 0,
   tooltipHover      : true,
-  content           : 'I love the challenge of figuring out interesting design layouts and endeavor to get better at css animations :)',
+  content           : 'I love modern web designs and like the challenge of figuring out interesting design layouts. Currently, I endeavor to get better at css animations.',
   ajaxContentUrl    : null,
   contentElementId  : null,
   useTitle          : false,
@@ -345,7 +345,7 @@ $('.jsSkill').tipso({
   offsetX           : 0,
   offsetY           : 0,
   tooltipHover      : true,
-  content           : 'Mainly I use it for DOM manipulations; mostly jQuery, but some vanilla too. However, I continue to study the language, currently: ES6 and React.',
+  content           : 'Mainly I use it for DOM manipulations and plugin control; mostly jQuery, but some vanilla too. I continue to study the language to evolve into a better programmer. Currently, I focus on ES6 and React.',
   ajaxContentUrl    : null,
   contentElementId  : null,
   useTitle          : false,
@@ -373,7 +373,7 @@ $('.wpSkill').tipso({
   offsetX           : 0,
   offsetY           : 0,
   tooltipHover      : true,
-  content           : 'I have been building custom Wordpress themes for a couple years now and enjoy the platform. It inspires me to get better at PHP as well as the WP ecosystem.',
+  content           : 'I have been building custom Wordpress themes for a few years now and enjoy the platform. It inspires me to get better at PHP as well as the WP ecosystem.',
   ajaxContentUrl    : null,
   contentElementId  : null,
   useTitle          : false,
@@ -382,9 +382,90 @@ $('.wpSkill').tipso({
   onShow            : null,
   onHide            : null
 });
-
-
-
+// BS Tooltip
+$('.bsSkill').tipso({
+  speed             : 400,        
+  background        : '#5b4386',
+  titleBackground   : '#333333',
+  color             : '#ffffff',
+  titleColor        : '#ffffff',
+  titleContent      : 'Bootstrap',       
+  showArrow         : true,
+  position          : 'bottom',
+  width             : 200,
+  maxWidth          : '',
+  delay             : 200,
+  hideDelay         : 0,
+  animationIn       : 'bounceIn',
+  animationOut      : 'bounceOut',
+  offsetX           : 0,
+  offsetY           : 0,
+  tooltipHover      : true,
+  content           : 'Initially, I used Foundation as it felt leaner, but I have come to savor the bootstrap framework and the efficiency it brings to a web project. I use it almost exclusively now.',
+  ajaxContentUrl    : null,
+  contentElementId  : null,
+  useTitle          : false,
+  templateEngineFunc: null,
+  onBeforeShow      : null,
+  onShow            : null,
+  onHide            : null
+});
+// PS Tooltip
+$('.psSkill').tipso({
+  speed             : 400,        
+  background        : '#29a4c6',
+  titleBackground   : '#333333',
+  color             : '#ffffff',
+  titleColor        : '#ffffff',
+  titleContent      : 'Photoshop',       
+  showArrow         : true,
+  position          : 'bottom',
+  width             : 200,
+  maxWidth          : '',
+  delay             : 200,
+  hideDelay         : 0,
+  animationIn       : 'bounceIn',
+  animationOut      : 'bounceOut',
+  offsetX           : 0,
+  offsetY           : 0,
+  tooltipHover      : true,
+  content           : 'A long time Photoshop user who relishes continually learning about the tool and refining my techniques.',
+  ajaxContentUrl    : null,
+  contentElementId  : null,
+  useTitle          : false,
+  templateEngineFunc: null,
+  onBeforeShow      : null,
+  onShow            : null,
+  onHide            : null
+});
+// IL Tooltip
+$('.ilSkill').tipso({
+  speed             : 400,        
+  background        : '#d76b15',
+  titleBackground   : '#333333',
+  color             : '#ffffff',
+  titleColor        : '#ffffff',
+  titleContent      : 'Illustrator',       
+  showArrow         : true,
+  position          : 'bottom',
+  width             : 200,
+  maxWidth          : '',
+  delay             : 200,
+  hideDelay         : 0,
+  animationIn       : 'bounceIn',
+  animationOut      : 'bounceOut',
+  offsetX           : 0,
+  offsetY           : 0,
+  tooltipHover      : true,
+  content           : 'A usefull tool which I have become more proficient with as web design has embraced Flat UI and iconography as well as modern browsers offering SVG capability.',
+  ajaxContentUrl    : null,
+  contentElementId  : null,
+  useTitle          : false,
+  templateEngineFunc: null,
+  onBeforeShow      : null,
+  onShow            : null,
+  onHide            : null
+});
 
 // Map
 $('#mapDiv').append(googleMap);
